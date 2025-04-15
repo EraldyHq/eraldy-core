a9ae1fee202b8d0366406b860657a492ee1b84fa
Nicolas GERARD <gerardnico@gmail.com>
2025-04-15 09:22:56 +0200

    fix(protector): deleted default passphrase functionality

--- core/src/main/java/net/bytle/crypto/Protector.java
+++ core/src/main/java/net/bytle/crypto/Protector.java
@@ -22,8 +22,6 @@ public class Protector {
 
   public static final String SEPARATOR = ">";
   public static final Charset UTF_8 = StandardCharsets.UTF_8;
-  public static final String DEFAULT_PASS = "r1zilGx22kRCUFjPGXbo";
-  private final boolean defaultPassphrase;
 
 
   private final String passphrase;
@@ -34,13 +32,8 @@ public class Protector {
    */
   private Protector(String passphrase) {
 
-    if (passphrase==null){
-      this.passphrase = DEFAULT_PASS;
-      this.defaultPassphrase = true;
-    } else {
-      this.passphrase = passphrase;
-      this.defaultPassphrase = false;
-    }
+    assert passphrase != null;
+    this.passphrase = passphrase;
 
   }
 
@@ -57,55 +50,48 @@ public class Protector {
    * @return a ciphertext with the following structure
    */
   public String encrypt(CryptoSymmetricCipher typeTransformationEnum, String plaintext) {
+
     if (plaintext == null) {
       return null;
-    } else {
-      String storedCryptoMaterial = typeTransformationEnum.encrypt(passphrase, plaintext);
-
-      // Encoding the transformation implementation (One bytes = 255 possible implementation)
-      // This byte gives the transformation version, there is for now only one
-      // The transformation version byte should always be first
-      byte[] transformationId = {typeTransformationEnum.getTransformationId().byteValue()};
-
-      String base64Concat = Base64Utility.bytesToBase64String(transformationId) + SEPARATOR + storedCryptoMaterial;
-      /**
-       * Two times to delete the SEPARATOR
-       */
-      return Base64Utility.bytesToBase64String(base64Concat.getBytes(UTF_8));
     }
+
+    String storedCryptoMaterial = typeTransformationEnum.encrypt(passphrase, plaintext);
+
+    // Encoding the transformation implementation (One bytes = 255 possible implementation)
+    // This byte gives the transformation version, there is for now only one
+    // The transformation version byte should always be first
+    byte[] transformationId = {typeTransformationEnum.getTransformationId().byteValue()};
+
+    String base64Concat = Base64Utility.bytesToBase64String(transformationId) + SEPARATOR + storedCryptoMaterial;
+    /**
+     * Two times to delete the SEPARATOR
+     */
+    return Base64Utility.bytesToBase64String(base64Concat.getBytes(UTF_8));
+
   }
 
-  public String decrypt(String storedEncryptedText){
-    return decrypt(storedEncryptedText,this.passphrase);
+  public String decrypt(String storedEncryptedText) {
+
+    return decrypt(storedEncryptedText, this.passphrase);
+
   }
 
   private String decrypt(String storedEncryptedText, String passphrase) {
+
     if (storedEncryptedText == null) {
-
       return null;
-
-    } else {
-
-      byte[] firstBytes = Base64Utility.base64StringToByte(storedEncryptedText);
-      String base64ConcatString = new String(firstBytes, 0, firstBytes.length, UTF_8);
-      String[] components = base64ConcatString.split(SEPARATOR, 2);
-
-      // First byte is the version of the crypto transformation
-      final int protectorStorageVersion = Byte.toUnsignedInt(Base64Utility.base64StringToByte(components[0])[0]);
-      CryptoSymmetricCipher typeTransformationEnum = CryptoSymmetricCipher.getFrom(protectorStorageVersion);
-
-      return typeTransformationEnum.decrypt(passphrase, components[1]);
-
     }
-  }
 
+    byte[] firstBytes = Base64Utility.base64StringToByte(storedEncryptedText);
+    String base64ConcatString = new String(firstBytes, 0, firstBytes.length, UTF_8);
+    String[] components = base64ConcatString.split(SEPARATOR, 2);
 
-  public String decryptWithDefault(String valueToDecrypt) {
-    return decrypt(valueToDecrypt,DEFAULT_PASS);
-  }
+    // First byte is the version of the crypto transformation
+    final int protectorStorageVersion = Byte.toUnsignedInt(Base64Utility.base64StringToByte(components[0])[0]);
+    CryptoSymmetricCipher typeTransformationEnum = CryptoSymmetricCipher.getFrom(protectorStorageVersion);
 
-  public boolean useDefaultPassphrase() {
-    return this.defaultPassphrase;
+    return typeTransformationEnum.decrypt(passphrase, components[1]);
+
   }
 
 
