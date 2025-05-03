02ad99fb9fdc07a835ba3fe9453e544af23b53db
Nicolas GERARD <gerardnico@gmail.com>
2025-05-03 17:03:15 +0200

    fix: emails are driver, not names

--- eraldy-smtp-client/src/main/java/net/bytle/email/BMailAddressStatic.java
+++ eraldy-smtp-client/src/main/java/net/bytle/email/BMailAddressStatic.java
@@ -14,7 +14,6 @@ import java.util.Map;
 public class BMailAddressStatic {
 
   /**
-   *
    * @param addressesList - a list of addresses separated by a comma
    * @return a list of internet address object
    * @throws AddressException - if the address is bad
@@ -24,10 +23,9 @@ public class BMailAddressStatic {
   }
 
   /**
-   *
    * @param addressAndNames - a map of addresses and corresponding name
    * @return a list of internet address object
-   * @throws AddressException - if the address is bad
+   * @throws AddressException             - if the address is bad
    * @throws UnsupportedEncodingException - if the name encoding can not be performed
    */
   public static List<InternetAddress> mapToListInternetAddress(Map<String, String> addressAndNames) throws AddressException, UnsupportedEncodingException {
@@ -41,11 +39,10 @@ public class BMailAddressStatic {
   }
 
   /**
-   *
-   * @param addresses - a list of addresses
+   * @param addresses    - a list of addresses
    * @param addressNames - a list of names
    * @return a list of internet address object
-   * @throws AddressException - if the address is bad
+   * @throws AddressException             - if the address is bad
    * @throws UnsupportedEncodingException - if the name encoding can not be performed
    */
   public static List<InternetAddress> addressAndNamesToListInternetAddress(String addresses, String addressNames) throws AddressException, UnsupportedEncodingException {
@@ -55,9 +52,15 @@ public class BMailAddressStatic {
     List<InternetAddress> internetAddresses = Arrays.asList(InternetAddress.parse(addresses));
     if (addressNames != null) {
       List<String> internetAddressesNames = Arrays.asList(addressNames.split(";"));
-      for (int i = 0; i < internetAddressesNames.size(); i++) {
-        String name = internetAddressesNames.get(i);
-        internetAddresses.get(i).setPersonal(name, String.valueOf(StandardCharsets.UTF_8));
+      for (int i = 0; i < internetAddresses.size(); i++) {
+        String name = null;
+        try {
+          name = internetAddressesNames.get(i);
+        } catch (IndexOutOfBoundsException e) {
+          // no name
+        }
+        InternetAddress internetAddress = internetAddresses.get(i);
+        internetAddress.setPersonal(name, String.valueOf(StandardCharsets.UTF_8));
       }
     }
     return internetAddresses;
@@ -65,7 +68,7 @@ public class BMailAddressStatic {
 
   public static InternetAddress addressAndNameToInternetAddress(String address, String name) throws AddressException, UnsupportedEncodingException {
     InternetAddress internetAddress = new InternetAddress(address);
-    if(name!=null) {
+    if (name != null) {
       internetAddress.setPersonal(name, String.valueOf(StandardCharsets.UTF_8));
     }
     return internetAddress;
